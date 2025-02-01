fc065a695794da4f5e5484ff900c6d40dff72e44
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2025-01-31 20:48:17 -0400

    Write schema files on run

--- src/gen-schema.rs
+++ src/gen-schema.rs
@@ -1,13 +1,13 @@
 mod models;
 use std::env;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let schema = if args.contains(&"--shared".to_string()) {
-        schemars::schema_for!(models::dependency::SharedPackageConfig)
-    } else {
-        schemars::schema_for!(models::package::PackageConfig)
-    };
 
-    println!("{}", serde_json::to_string_pretty(&schema).unwrap());
+fn main() {
+    let shared_schema_json = schemars::schema_for!(models::dependency::SharedPackageConfig);
+    let shared_schema = serde_json::to_string_pretty(&shared_schema_json).unwrap();
+    std::fs::write("qpm.shared.schema.json", shared_schema).expect("Failed to write shared schema");
+
+    let schema_json = schemars::schema_for!(models::package::PackageConfig);
+    let schema = serde_json::to_string_pretty(&schema_json).unwrap();
+    std::fs::write("qpm.schema.json", schema).expect("Failed to write schema");
 }
