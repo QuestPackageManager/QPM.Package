9fa5ba5e3eb06f15531c8e5b7c7b7b6d5c68f023
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2025-07-28 18:16:06 -0400

    Refactor QPkg struct to include PackageConfig and update schemars descriptions

--- src/models/qpkg.rs
+++ src/models/qpkg.rs
@@ -2,7 +2,7 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, path::PathBuf};
 
-use crate::models::triplet::TripletId;
+use crate::models::{package::PackageConfig, triplet::TripletId};
 
 pub const QPKG_JSON: &str = "qpm2.qpkg.json";
 
@@ -15,10 +15,16 @@ pub const QPKG_JSON: &str = "qpm2.qpkg.json";
     description = "QPKG package. Distributes a package with all triplet binaries and their headers."
 )]
 pub struct QPkg {
+    /// Package configuration
+    #[schemars(description = "Package configuration")]
+    pub config: PackageConfig,
+
     /// The directory where the headers are located
+    #[schemars(description = "The directory where the headers are located")]
     pub shared_dir: PathBuf,
 
     /// Triplet map
+    #[schemars(description = "Triplet map")]
     pub triplets: HashMap<TripletId, QPkgTripletInfo>,
 }
 
